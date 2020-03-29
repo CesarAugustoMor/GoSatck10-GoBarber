9057c66cf494aa5b89e582020a6cdf4069cc3d2e
CesarAugustoMor <cesaramfurst@gmail.com>
2020-03-29 16:47:33 -0300

    Listagem de provedores

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-
-import multerConfig from './config/multer';
-import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
+import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
 
@@ -16,7 +17,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 const upload = multer(multerConfig);
+routes.get('/providers', ProviderController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
