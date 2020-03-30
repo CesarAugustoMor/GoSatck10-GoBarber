406933059a5f3086aff9404994c242da154a586f
CesarAugustoMor <cesaramfurst@gmail.com>
2020-03-30 16:59:53 -0300

    Configuração para envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '6837d0e1a87aa0',
+    pass: '33226fc6212ffa',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
